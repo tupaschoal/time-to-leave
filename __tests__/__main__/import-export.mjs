dfbfacff82ce6359248ed43b26b0221f03bf7224
Arthur Araujo <araujoarthur0@hotmail.com>
2025-01-14 21:14:39 -0300

    Fixing import-export test to avoid leaving folder in the root

--- __tests__/__main__/import-export.mjs
+++ __tests__/__main__/import-export.mjs
@@ -9,6 +9,39 @@ import ImportExport from '../../js/import-export.mjs';
 
 describe('Import export', function()
 {
+    const folder = fs.mkdtempSync('import-export');
+    const invalidEntriesFile = path.join(folder, 'invalid.ttldb');
+
+    const calendarStore = new Store({name: 'flexible-store'});
+    const waivedWorkdays = new Store({name: 'waived-workdays'});
+
+    before(() =>
+    {
+        calendarStore.clear();
+        const entries = {
+            '2020-3-1': {'values': ['08:00', '12:00', '13:00', '17:00']},
+            '2020-3-2': {'values': ['07:00', '11:00', '14:00', '18:00']}
+        };
+        calendarStore.set(entries);
+
+        waivedWorkdays.clear();
+        const waivedEntries = {
+            '2019-12-31': {reason: 'New Year\'s eve', hours: '08:00'},
+            '2020-01-01': {reason: 'New Year\'s Day', hours: '08:00'},
+            '2020-04-10': {reason: 'Good Friday', hours: '08:00'}
+        };
+        waivedWorkdays.set(waivedEntries);
+
+        const invalidEntriesContent =
+        `[{"type": "flexible", "date": "not-a-date", "data": "day-begin", "hours": "08:00"},
+          {"type": "waived", "date": "2020-01-01", "data": "example waiver 2", "hours": "not-an-hour"},
+          {"type": "flexible", "date": "not-a-date", "data": "day-end", "hours": "17:00"},
+          {"type": "flexible", "date": "not-a-date", "values": "not-an-array"},
+          {"type": "not-a-type", "date": "not-a-date", "data": "day-end", "hours": "17:00"}
+         ]`;
+        fs.writeFileSync(invalidEntriesFile, invalidEntriesContent, 'utf8');
+    });
+
     describe('validEntry(entry)', function()
     {
         const goodEntry = {'type': 'flexible', 'date': '2020-06-03', 'values': ['08:00', '12:00', '13:00', '14:00']};
@@ -30,26 +63,6 @@ describe('Import export', function()
         });
     });
 
-    const calendarStore = new Store({name: 'flexible-store'});
-    const waivedWorkdays = new Store({name: 'waived-workdays'});
-
-    calendarStore.clear();
-    const entries = {
-        '2020-3-1': {'values': ['08:00', '12:00', '13:00', '17:00']},
-        '2020-3-2': {'values': ['07:00', '11:00', '14:00', '18:00']}
-    };
-    calendarStore.set(entries);
-
-    waivedWorkdays.clear();
-    const waivedEntries = {
-        '2019-12-31': {reason: 'New Year\'s eve', hours: '08:00'},
-        '2020-01-01': {reason: 'New Year\'s Day', hours: '08:00'},
-        '2020-04-10': {reason: 'Good Friday', hours: '08:00'}
-    };
-    waivedWorkdays.set(waivedEntries);
-
-    const folder = fs.mkdtempSync('import-export');
-
     describe('exportDatabaseToFile', function()
     {
         it('Check that export works', () =>
@@ -58,16 +71,6 @@ describe('Import export', function()
             assert.strictEqual(ImportExport.exportDatabaseToFile('/not/a/valid/path'), false);
         });
     });
-
-    const invalidEntriesContent =
-        `[{"type": "flexible", "date": "not-a-date", "data": "day-begin", "hours": "08:00"},
-          {"type": "waived", "date": "2020-01-01", "data": "example waiver 2", "hours": "not-an-hour"},
-          {"type": "flexible", "date": "not-a-date", "data": "day-end", "hours": "17:00"},
-          {"type": "flexible", "date": "not-a-date", "values": "not-an-array"},
-          {"type": "not-a-type", "date": "not-a-date", "data": "day-end", "hours": "17:00"}
-         ]`;
-    const invalidEntriesFile = path.join(folder, 'invalid.ttldb');
-    fs.writeFileSync(invalidEntriesFile, invalidEntriesContent, 'utf8');
 
     describe('importDatabaseFromFile', function()
     {
